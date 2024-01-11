24ce41fa906a6eb3619cc9562332eb40cb17e7e6
Caleb Hemphill <calebhemp@gmail.com>
2024-01-11 00:30:37 -0500

    Create function to parse template lines

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -5,8 +5,35 @@ def read_template(file_path):
 
   return template
 
-def parse_template():
-  pass
+def parse_template(template):
+  
+  template_list = template.split()
+  parts = []
+
+  for i, word in enumerate(template_list):
+    if word[0] == '{':
+      end = word.find('}')
+      part = word[1:end]
+      parts.append(part)
+      stripped = word[0] + word[end:]
+      template_list[i] = stripped
+
+  template_stripped = ' '.join(template_list)
+  template_parts = tuple(parts)
+  parsed_template = (template_stripped, template_parts)
+
+  return parsed_template
 
 def merge():
   pass
+
+
+# def test():
+
+#   template = read_template('../assets/dark_and_stormy_night_template.txt')
+
+#   parsed = parse_template(template)
+
+#   return parsed
+
+# print(test())
