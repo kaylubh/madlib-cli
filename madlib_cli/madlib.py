3d9aa85d861e123b02acce71eb48ce900bd555d0
Caleb Hemphill <calebhemp@gmail.com>
2024-01-10 23:00:47 -0500

    Create function to read template files

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,12 @@
+def read_template(file_path):
+
+  with open(file_path) as template_file:
+    template = template_file.read()
+
+  return template
+
+def parse_template():
+  pass
+
+def merge():
+  pass
